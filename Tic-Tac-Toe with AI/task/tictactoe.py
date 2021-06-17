354f4b296c7fbab077166b8bc65e3bf08f4c3378
jkolda <jkolda@casaleproject.cz>
2021-06-17 12:46:30 +0200

    Stage 1 finished

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -1,5 +1,6 @@
 # write your code here
-from numpy import empty, reshape
+from numpy import empty, reshape, ndarray
+from itertools import combinations
 
 
 class TicTacToe:
@@ -7,8 +8,12 @@ class TicTacToe:
         self.field = empty((rows, columns), dtype=str, order='F')
         self.rows = rows
         self.columns = columns
+        self.player = None
 
-    def start_setup(self, *setup):
+    def current_player(self):
+        return 'X' if (self.field == 'O').sum() >= (self.field == 'X').sum() else 'O'
+
+    def start_setup(self, *setup: str):
         if not setup:
             while True:
                 setup = input('Input starting field state:\n')
@@ -19,21 +24,57 @@ class TicTacToe:
                 else:
                     break
         self.field = reshape(list(setup), (self.columns, self.rows))
+        self.field[self.field == '_'] = ' '
 
     def print_field(self):
-        print(*self.field, sep='\n')
+        print(9 * '-')
+        for i in range(self.rows):
+            print('|', *self.field[i], '|', sep=' ')
+        print(9 * '-')
+
+    def evaluate_game(self):
+        winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        flat_field = self.field.flatten().tolist()
+        indices = tuple(i for i in range(len(flat_field)) if flat_field[i] == self.player)
+        if any([i in winners for i in list(combinations(indices, 3))]):
+            print(f'{self.player} wins')
+            quit()
+        elif ' ' not in flat_field:
+            print('Draw')
+            quit()
+        else:
+            print('Game not finished')
 
     def ask4move(self):
+        self.player = self.current_player()
         while True:
-            row_pos, col_pos = map(int, input('Enter the coordinates: ').split(' '))
-            row_pos -= 1
-            col_pos -= 1
+            move = input('Enter the coordinates: ')
+            if move == 'exit':
+                quit()
+            try:
+                row_pos, col_pos = map(int, move.split(' '))
+                row_pos -= 1
+                col_pos -= 1
 
-            if self.field[col_pos, row_pos] != '_':
-                print
+                if (row_pos > self.rows - 1) or (col_pos > self.columns - 1):
+                    print(f'Coordinates should be from 1 to {self.columns}!')
+                elif self.field[row_pos, col_pos] != ' ':
+                    print('This cell is occupied! Choose another one!')
+                else:
+                    self.field[row_pos, col_pos] = self.player
+                    self.print_field()
+                    break
+            except ValueError:
+                print('You should enter numbers!')
 
 
 if __name__ == '__main__':
     a = TicTacToe()
-    a.start_setup(list('XXXOOO___'))
+    # a.start_setup(list('_XXOO_OX_'))
+    a.start_setup()
+    a.print_field()
+    while True:
+        a.ask4move()
+        a.evaluate_game()
+
 
